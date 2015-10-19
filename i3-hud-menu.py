ec73883a9e2da1013d8c3c932b3270d654f971e0
JAMcNaughton <jamcnaughton@users.noreply.github.com>
2015-10-19 08:23:07 +0100

    Updated i3-hud-menu.py
    - Made to work with xubuntu 14.04 (by using rstrip on menu items)
    - 'Root > ' removed from menu items in formatting.
    - Underscores removed from menu items in formatting (may be a bit heavy handed)
    - Menu items sorted alphabetically to ensure consistent appearance
    - Menu items with no action not shown in dmenu (for both general and gtk apps)
    - Check in gtk menu population for actions with no label (allows apps like Eclipse to use the HUD)

--- i3-hud-menu.py
+++ i3-hud-menu.py
@@ -11,6 +11,8 @@ def format_label_list(label_list):
   result = head
   for label in tail:
     result = result + " > " + label
+  result = result.replace("Root > ", "")
+  result = result.replace("_", "")
   return result
 
 """
@@ -35,10 +37,14 @@ def try_appmenu_interface(window_id):
 
   dbusmenu_item_dict = dict()
 
+  #For excluding items which have no action
+  blacklist = [] 
+
   """ explore_dbusmenu_item """
   def explore_dbusmenu_item(item, label_list):
     item_id = item[0]
     item_props = item[1]
+
     item_children = item[2]
 
     if 'label' in item_props:
@@ -46,32 +52,35 @@ def try_appmenu_interface(window_id):
     else:
       new_label_list = label_list
 
-    # FIXME: This is not excluding all unactivable menuitems.
     if len(item_children) == 0:
-      dbusmenu_item_dict[format_label_list(new_label_list)] = item_id
+      if new_label_list not in blacklist:
+        dbusmenu_item_dict[format_label_list(new_label_list)] = item_id
     else:
+      blacklist.append(new_label_list)
       for child in item_children:
         explore_dbusmenu_item(child, new_label_list)
 
   explore_dbusmenu_item(dbusmenu_items[1], [])
 
+  dmenuKeys = sorted(dbusmenu_item_dict.keys())
+
   # --- Run dmenu
   dmenu_string = ''
-  head, *tail = dbusmenu_item_dict.keys()
+  head, *tail = dmenuKeys
   dmenu_string = head
   for m in tail:
     dmenu_string += '\n'
     dmenu_string += m
 
-  dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '10'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+  dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '15'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
   dmenu_cmd.stdin.write(dmenu_string.encode('utf-8'))
-  dmenu_result = dmenu_cmd.communicate()[0].decode('utf8')
+  dmenu_result = dmenu_cmd.communicate()[0].decode('utf8').rstrip()
   dmenu_cmd.stdin.close()
 
   # --- Use dmenu result
   if dmenu_result in dbusmenu_item_dict:
     action = dbusmenu_item_dict[dmenu_result]
-    print(dbusmenu_object_iface.Event(action, 'clicked', 0, 0))
+    # print(dbusmenu_object_iface.Event(action, 'clicked', 0, 0))
 
 
 """
@@ -79,9 +88,9 @@ def try_appmenu_interface(window_id):
 """
 def try_gtk_interface(gtk_bus_name_cmd, gtk_object_path_cmd):
   gtk_bus_name = gtk_bus_name_cmd.split(' ')[2].split('\n')[0].split('"')[1]
-  print(gtk_object_path_cmd)
+  # print(gtk_object_path_cmd)
   gtk_object_path = gtk_object_path_cmd.split(' ')[2].split('\n')[0].split('"')[1]
-  print("GTK MenuModel Bus name and object path: ", gtk_bus_name, gtk_object_path)
+  # print("GTK MenuModel Bus name and object path: ", gtk_bus_name, gtk_object_path)
 
   # --- Ask for menus over DBus ---
   session_bus = dbus.SessionBus()
@@ -96,55 +105,59 @@ def try_gtk_interface(gtk_bus_name_cmd, gtk_object_path_cmd):
     gtk_menubar_menus[(gtk_menubar_result[0], gtk_menubar_result[1])] = gtk_menubar_result[2]
 
   gtk_menubar_action_dict = dict()
-  gtk_menubar_target_dict = dict()
+ # gtk_menubar_target_dict = dict()
 
   """ explore_menu """
   def explore_menu(menu_id, label_list):
-    for menu in gtk_menubar_menus[menu_id]:
-      if 'label' in menu:
-        menu_label = menu['label'].replace('_', '')
-      else:
-        menu_label = '?'
+    if menu_id in gtk_menubar_menus:
+      for menu in gtk_menubar_menus[menu_id]:
+        if 'label' in menu:
+          menu_label = menu['label'].replace('_', '')
+        else:
+          menu_label = '?'
 
-      new_label_list = label_list + [menu_label]
-      formatted_label = format_label_list(new_label_list)
+        new_label_list = label_list + [menu_label]
+        formatted_label = format_label_list(new_label_list)
 
-      if 'action' in menu:
-        menu_action = menu['action']
-        gtk_menubar_action_dict[formatted_label] = menu_action
-        if 'target' in menu:
-          menu_target = menu['target']
-          gtk_menubar_target_dict[formatted_label] = menu_target
+        if 'action' in menu:
+          menu_action = menu['action']
+          if ':section' not in menu and ':submenu' not in menu:
+            gtk_menubar_action_dict[formatted_label] = menu_action
+          # if 'target' in menu:
+           # menu_target = menu['target']
+           # gtk_menubar_target_dict[formatted_label] = menu_target
 
-      if ':section' in menu:
-        menu_section = menu[':section']
-        section_menu_id = (menu_section[0], menu_section[1])
-        explore_menu(section_menu_id, label_list)
+        if ':section' in menu:
+          menu_section = menu[':section']
+          section_menu_id = (menu_section[0], menu_section[1])
+          explore_menu(section_menu_id, label_list)
 
-      if ':submenu' in menu:
-        menu_submenu = menu[':submenu']
-        submenu_menu_id = (menu_submenu[0], menu_submenu[1])
-        explore_menu(submenu_menu_id, new_label_list)
+        if ':submenu' in menu:
+          menu_submenu = menu[':submenu']
+          submenu_menu_id = (menu_submenu[0], menu_submenu[1])
+          explore_menu(submenu_menu_id, new_label_list)
 
   explore_menu((0,0), [])
 
+  dmenuKeys = sorted(gtk_menubar_action_dict.keys())
+
   # --- Run dmenu
   dmenu_string = ''
-  head, *tail = gtk_menubar_action_dict.keys()
+  head, *tail = dmenuKeys
   dmenu_string = head
   for m in tail:
     dmenu_string += '\n'
     dmenu_string += m
 
-  dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '10'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+  dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '15'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
   dmenu_cmd.stdin.write(dmenu_string.encode('utf-8'))
-  dmenu_result = dmenu_cmd.communicate()[0].decode('utf8')
+  dmenu_result = dmenu_cmd.communicate()[0].decode('utf8').rstrip()
   dmenu_cmd.stdin.close()
 
   # --- Use dmenu result
   if dmenu_result in gtk_menubar_action_dict:
     action = gtk_menubar_action_dict[dmenu_result]
-    print('GTK Action :', action)
+    # print('GTK Action :', action)
     gtk_action_object_actions_iface.Activate(action.replace('unity.', ''), [], dict())
 
 """
@@ -153,14 +166,17 @@ def try_gtk_interface(gtk_bus_name_cmd, gtk_object_path_cmd):
 
 # --- Get X Window ID ---
 window_id_cmd = subprocess.check_output(['xprop', '-root', '-notype', '_NET_ACTIVE_WINDOW']).decode('utf-8')
-window_id = window_id_cmd.split(' ')[4].split('\n')[0]
+window_id = window_id_cmd.split(' ')[4].split(',')[0].split('\n')[0]
 
-print('Window id is :', window_id)
+# For testing get the id of the target window from xwininfo
+# window_id = '0x3400186'
+# print('Window id is :', window_id)
 
 # --- Get GTK MenuModel Bus name ---
-
 gtk_bus_name_cmd = subprocess.check_output(['xprop', '-id', window_id, '-notype', '_GTK_UNIQUE_BUS_NAME']).decode('utf-8')
 gtk_object_path_cmd = subprocess.check_output(['xprop', '-id', window_id, '-notype', '_GTK_MENUBAR_OBJECT_PATH']).decode('utf-8')
+
+# print(gtk_bus_name_cmd)
 
 if gtk_bus_name_cmd == '_GTK_UNIQUE_BUS_NAME:  not found.\n' or gtk_object_path_cmd == '_GTK_MENUBAR_OBJECT_PATH:  not found.\n':
   try_appmenu_interface(int(window_id, 16))
