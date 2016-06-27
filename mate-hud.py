f33e762b521169144cbc2b6f8bf11b166dd779b5
Martin Wimpress <code@flexion.org>
2016-06-27 11:15:14 +0100

    Correct indentation.

--- mate-hud.py
+++ mate-hud.py
@@ -73,163 +73,162 @@ class EWMH:
   format_label_list
 """
 def format_label_list(label_list):
-  head, *tail = label_list
-  result = head
-  for label in tail:
-    result = result + ' > ' + label
-  return result.replace('Root > ', '').replace('_', '')
+    head, *tail = label_list
+    result = head
+    for label in tail:
+        result = result + ' > ' + label
+
+    return result.replace('Root > ', '').replace('_', '')
 
 """
   try_appmenu_interface
 """
 def try_appmenu_interface(window_id):
-  # --- Get Appmenu Registrar DBus interface
-  session_bus = dbus.SessionBus()
-  appmenu_registrar_object = session_bus.get_object('com.canonical.AppMenu.Registrar', '/com/canonical/AppMenu/Registrar')
-  appmenu_registrar_object_iface = dbus.Interface(appmenu_registrar_object, 'com.canonical.AppMenu.Registrar')
+    # --- Get Appmenu Registrar DBus interface
+    session_bus = dbus.SessionBus()
+    appmenu_registrar_object = session_bus.get_object('com.canonical.AppMenu.Registrar', '/com/canonical/AppMenu/Registrar')
+    appmenu_registrar_object_iface = dbus.Interface(appmenu_registrar_object, 'com.canonical.AppMenu.Registrar')
 
-  # --- Get dbusmenu object path
-  try:
-    dbusmenu_bus, dbusmenu_object_path = appmenu_registrar_object_iface.GetMenuForWindow(window_id)
-  except dbus.exceptions.DBusException:
-    return
+    # --- Get dbusmenu object path
+    try:
+        dbusmenu_bus, dbusmenu_object_path = appmenu_registrar_object_iface.GetMenuForWindow(window_id)
+    except dbus.exceptions.DBusException:
+        return
 
-  # --- Access dbusmenu items
-  dbusmenu_object = session_bus.get_object(dbusmenu_bus, dbusmenu_object_path)
-  dbusmenu_object_iface = dbus.Interface(dbusmenu_object, 'com.canonical.dbusmenu')
-  dbusmenu_items = dbusmenu_object_iface.GetLayout(0, -1, ["label"])
+    # --- Access dbusmenu items
+    dbusmenu_object = session_bus.get_object(dbusmenu_bus, dbusmenu_object_path)
+    dbusmenu_object_iface = dbus.Interface(dbusmenu_object, 'com.canonical.dbusmenu')
+    dbusmenu_items = dbusmenu_object_iface.GetLayout(0, -1, ["label"])
+    dbusmenu_item_dict = dict()
 
-  dbusmenu_item_dict = dict()
+    #For excluding items which have no action
+    blacklist = []
 
-  #For excluding items which have no action
-  blacklist = []
+    """ explore_dbusmenu_item """
+    def explore_dbusmenu_item(item, label_list):
+        item_id = item[0]
+        item_props = item[1]
 
-  """ explore_dbusmenu_item """
-  def explore_dbusmenu_item(item, label_list):
-    item_id = item[0]
-    item_props = item[1]
+        item_children = item[2]
 
-    item_children = item[2]
+        if 'label' in item_props:
+            new_label_list = label_list + [item_props['label']]
+        else:
+            new_label_list = label_list
 
-    if 'label' in item_props:
-      new_label_list = label_list + [item_props['label']]
-    else:
-      new_label_list = label_list
+        if len(item_children) == 0:
+            if new_label_list not in blacklist:
+                dbusmenu_item_dict[format_label_list(new_label_list)] = item_id
+        else:
+            blacklist.append(new_label_list)
+            for child in item_children:
+                explore_dbusmenu_item(child, new_label_list)
 
-    if len(item_children) == 0:
-      if new_label_list not in blacklist:
-        dbusmenu_item_dict[format_label_list(new_label_list)] = item_id
-    else:
-      blacklist.append(new_label_list)
-      for child in item_children:
-        explore_dbusmenu_item(child, new_label_list)
+    explore_dbusmenu_item(dbusmenu_items[1], [])
+    dmenuKeys = sorted(dbusmenu_item_dict.keys())
 
-  explore_dbusmenu_item(dbusmenu_items[1], [])
+    # --- Run dmenu
+    dmenu_string = ''
+    head, *tail = dmenuKeys
+    dmenu_string = head
+    for m in tail:
+        dmenu_string += '\n'
+        dmenu_string += m
 
-  dmenuKeys = sorted(dbusmenu_item_dict.keys())
+    dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '15'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    dmenu_cmd.stdin.write(dmenu_string.encode('utf-8'))
+    dmenu_result = dmenu_cmd.communicate()[0].decode('utf8').rstrip()
+    dmenu_cmd.stdin.close()
 
-  # --- Run dmenu
-  dmenu_string = ''
-  head, *tail = dmenuKeys
-  dmenu_string = head
-  for m in tail:
-    dmenu_string += '\n'
-    dmenu_string += m
-
-  dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '15'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-  dmenu_cmd.stdin.write(dmenu_string.encode('utf-8'))
-  dmenu_result = dmenu_cmd.communicate()[0].decode('utf8').rstrip()
-  dmenu_cmd.stdin.close()
-
-  # --- Use dmenu result
-  if dmenu_result in dbusmenu_item_dict:
-    action = dbusmenu_item_dict[dmenu_result]
-    dbusmenu_object_iface.Event(action, 'clicked', 0, 0)
+    # --- Use dmenu result
+    if dmenu_result in dbusmenu_item_dict:
+        action = dbusmenu_item_dict[dmenu_result]
+        dbusmenu_object_iface.Event(action, 'clicked', 0, 0)
 
 
 """
   try_gtk_interface
 """
 def try_gtk_interface(gtk_bus_name, gtk_object_path):
-  # --- Ask for menus over DBus ---
-  session_bus = dbus.SessionBus()
-  gtk_menubar_object = session_bus.get_object(gtk_bus_name, gtk_object_path)
-  gtk_menubar_object_iface = dbus.Interface(gtk_menubar_object, dbus_interface='org.gtk.Menus')
-  gtk_action_object_actions_iface = dbus.Interface(gtk_menubar_object, dbus_interface='org.gtk.Actions')
-  gtk_menubar_results = gtk_menubar_object_iface.Start([x for x in range(1024)])
+    # --- Ask for menus over DBus ---
+    session_bus = dbus.SessionBus()
+    gtk_menubar_object = session_bus.get_object(gtk_bus_name, gtk_object_path)
+    gtk_menubar_object_iface = dbus.Interface(gtk_menubar_object, dbus_interface='org.gtk.Menus')
+    gtk_action_object_actions_iface = dbus.Interface(gtk_menubar_object, dbus_interface='org.gtk.Actions')
+    gtk_menubar_results = gtk_menubar_object_iface.Start([x for x in range(1024)])
 
-  # --- Construct menu list ---
-  gtk_menubar_menus = dict()
-  for gtk_menubar_result in gtk_menubar_results:
-    gtk_menubar_menus[(gtk_menubar_result[0], gtk_menubar_result[1])] = gtk_menubar_result[2]
+    # --- Construct menu list ---
+    gtk_menubar_menus = dict()
+    for gtk_menubar_result in gtk_menubar_results:
+        gtk_menubar_menus[(gtk_menubar_result[0], gtk_menubar_result[1])] = gtk_menubar_result[2]
 
-  gtk_menubar_action_dict = dict()
- # gtk_menubar_target_dict = dict()
+    gtk_menubar_action_dict = dict()
+    #gtk_menubar_target_dict = dict()
 
-  """ explore_menu """
-  def explore_menu(menu_id, label_list):
-    if menu_id in gtk_menubar_menus:
-      for menu in gtk_menubar_menus[menu_id]:
-        if 'label' in menu:
-          menu_label = menu['label']
-          new_label_list = label_list + [menu_label]
-          formatted_label = format_label_list(new_label_list)
+    """ explore_menu """
+    def explore_menu(menu_id, label_list):
+        if menu_id in gtk_menubar_menus:
+            for menu in gtk_menubar_menus[menu_id]:
+                if 'label' in menu:
+                    menu_label = menu['label']
+                    new_label_list = label_list + [menu_label]
+                    formatted_label = format_label_list(new_label_list)
 
-          if 'action' in menu:
-            menu_action = menu['action']
-            if ':section' not in menu and ':submenu' not in menu:
-              gtk_menubar_action_dict[formatted_label] = menu_action
-            # if 'target' in menu:
-             # menu_target = menu['target']
-             # gtk_menubar_target_dict[formatted_label] = menu_target
+                    if 'action' in menu:
+                        menu_action = menu['action']
+                        if ':section' not in menu and ':submenu' not in menu:
+                            gtk_menubar_action_dict[formatted_label] = menu_action
+                        #if 'target' in menu:
+                        #   menu_target = menu['target']
+                        #   gtk_menubar_target_dict[formatted_label] = menu_target
 
-        if ':section' in menu:
-          menu_section = menu[':section']
-          section_menu_id = (menu_section[0], menu_section[1])
-          explore_menu(section_menu_id, label_list)
+                if ':section' in menu:
+                    menu_section = menu[':section']
+                    section_menu_id = (menu_section[0], menu_section[1])
+                    explore_menu(section_menu_id, label_list)
 
-        if ':submenu' in menu:
-          menu_submenu = menu[':submenu']
-          submenu_menu_id = (menu_submenu[0], menu_submenu[1])
-          explore_menu(submenu_menu_id, new_label_list)
+                if ':submenu' in menu:
+                    menu_submenu = menu[':submenu']
+                    submenu_menu_id = (menu_submenu[0], menu_submenu[1])
+                    explore_menu(submenu_menu_id, new_label_list)
 
-  explore_menu((0,0), [])
+    explore_menu((0,0), [])
 
-  dmenuKeys = sorted(gtk_menubar_action_dict.keys())
+    dmenuKeys = sorted(gtk_menubar_action_dict.keys())
 
-  # --- Run dmenu
-  dmenu_string = ''
-  head, *tail = dmenuKeys
-  dmenu_string = head
-  for m in tail:
-    dmenu_string += '\n'
-    dmenu_string += m
+    # --- Run dmenu
+    dmenu_string = ''
+    head, *tail = dmenuKeys
+    dmenu_string = head
+    for m in tail:
+        dmenu_string += '\n'
+        dmenu_string += m
 
-  dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '15', '-fn', '"Ubuntu Regular-10.5"', '-nb', '#33322f', '-nf', '#cccccc', '-sf', '#cccccc', '-sb', '#87a752'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-  dmenu_cmd.stdin.write(dmenu_string.encode('utf-8'))
-  dmenu_result = dmenu_cmd.communicate()[0].decode('utf8').rstrip()
-  dmenu_cmd.stdin.close()
+    dmenu_cmd = subprocess.Popen(['dmenu', '-i', '-l', '15', '-fn', '"Ubuntu Regular-10.5"', '-nb', '#33322f', '-nf', '#cccccc', '-sf', '#cccccc', '-sb', '#87a752'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    dmenu_cmd.stdin.write(dmenu_string.encode('utf-8'))
+    dmenu_result = dmenu_cmd.communicate()[0].decode('utf8').rstrip()
+    dmenu_cmd.stdin.close()
 
-  # --- Use dmenu result
-  if dmenu_result in gtk_menubar_action_dict:
-    action = gtk_menubar_action_dict[dmenu_result]
-    # print('GTK Action :', action)
-    gtk_action_object_actions_iface.Activate(action.replace('unity.', ''), [], dict())
+    # --- Use dmenu result
+    if dmenu_result in gtk_menubar_action_dict:
+      action = gtk_menubar_action_dict[dmenu_result]
+      # print('GTK Action :', action)
+      gtk_action_object_actions_iface.Activate(action.replace('unity.', ''), [], dict())
 
 if __name__ == "__main__":
-  # Get Window properties and GTK MenuModel Bus name
-  ewmh = EWMH()
-  win = ewmh.getActiveWindow()
-  window_id = hex(ewmh._getProperty('_NET_ACTIVE_WINDOW')[0])
-  gtk_bus_name = ewmh._getProperty('_GTK_UNIQUE_BUS_NAME', win)
-  gtk_object_path = ewmh._getProperty('_GTK_MENUBAR_OBJECT_PATH', win)
-  print('Window id is :', window_id)
-  print('_GTK_UNIQUE_BUS_NAME: ' + gtk_bus_name)
-  print('_GTK_MENUBAR_OBJECT_PATH: ' + gtk_object_path)
+    # Get Window properties and GTK MenuModel Bus name
+    ewmh = EWMH()
+    win = ewmh.getActiveWindow()
+    window_id = hex(ewmh._getProperty('_NET_ACTIVE_WINDOW')[0])
+    gtk_bus_name = ewmh._getProperty('_GTK_UNIQUE_BUS_NAME', win)
+    gtk_object_path = ewmh._getProperty('_GTK_MENUBAR_OBJECT_PATH', win)
+    print('Window id is :', window_id)
+    print('_GTK_UNIQUE_BUS_NAME: ' + gtk_bus_name)
+    print('_GTK_MENUBAR_OBJECT_PATH: ' + gtk_object_path)
 
-  if (not gtk_bus_name) or (not gtk_object_path):
-    print('Trying AppMenu')
-    try_appmenu_interface(int(window_id, 16))
-  else:
-    print('Trying GTK interface')
-    try_gtk_interface(gtk_bus_name, gtk_object_path)
+    if (not gtk_bus_name) or (not gtk_object_path):
+        print('Trying AppMenu')
+        try_appmenu_interface(int(window_id, 16))
+    else:
+        print('Trying GTK interface')
+        try_gtk_interface(gtk_bus_name, gtk_object_path)
