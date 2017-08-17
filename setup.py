30533152fb6ec8434a027a87f92218561eb7fa64
Martin Wimpress <martin@ubuntu-mate.org>
2017-08-17 18:38:33 +0100

    Release 17.10.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import DistUtilsExtra.command.build_extra
 import DistUtilsExtra.command.build_i18n
 import DistUtilsExtra.command.clean_i18n
 
-__VERSION__ = '17.10.4'
+__VERSION__ = '17.10.5'
 
 def datafilelist(installbase, sourcebase):
     datafileList = []
