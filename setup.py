541dab90cf916763fcc7bb885c71cf1d57fb91ee
Martin Wimpress <martin@ubuntu-mate.org>
2018-01-22 23:44:57 +0000

    Release 18.04.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import DistUtilsExtra.command.build_extra
 import DistUtilsExtra.command.build_i18n
 import DistUtilsExtra.command.clean_i18n
 
-__VERSION__ = '18.04.2'
+__VERSION__ = '18.04.3'
 
 def datafilelist(installbase, sourcebase):
     datafileList = []
